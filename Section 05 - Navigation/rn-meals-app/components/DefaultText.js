3b4182a19907e1bfd97888fb8ce86ae59aacfa4b
Matthew McGrath <rubywep@hotmail.com>
2022-01-24 13:36:44 -0800

    Add optional 'bold' prop to DefaultText component to conditionally style the text as regular or bold.

--- Section 05 - Navigation/rn-meals-app/components/DefaultText.js
+++ Section 05 - Navigation/rn-meals-app/components/DefaultText.js
@@ -3,11 +3,14 @@ import React from 'react';
 
 // Simple wrapper component to give our text some default, universal styling
 const DefaultText = (props) => {
-  return <Text style={styles.text}>{props.children}</Text>;
+  return <Text style={props.bold ? styles.textBold : styles.textRegular}>{props.children}</Text>;
 };
 
 const styles = StyleSheet.create({
-  text: {
+  textBold: {
+    fontFamily: 'open-sans-bold',
+  },
+  textRegular: {
     fontFamily: 'open-sans',
   },
 });
