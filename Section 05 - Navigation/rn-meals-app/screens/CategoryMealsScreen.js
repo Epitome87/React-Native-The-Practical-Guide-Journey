384a951c79fdb929ced07cb91511a319f9fbb5b3
Matthew McGrath <rubywep@hotmail.com>
2022-01-27 13:48:53 -0800

    Add fallback display messages for when Filter Screen or Meal Detail Screen contain no Meals to display.

--- Section 05 - Navigation/rn-meals-app/screens/CategoryMealsScreen.js
+++ Section 05 - Navigation/rn-meals-app/screens/CategoryMealsScreen.js
@@ -1,23 +1,30 @@
 import React, { useEffect } from 'react';
+import { StyleSheet, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import { CATEGORIES } from '../data/dummy-data';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const CategoryMealsScreen = ({ navigation, route }) => {
+  // We navigated to this Screen via: navigation.navigate('CategoryMeals', { categoryId: itemData.item.id });
+  // This gives us access to categoryId in our route.params object
   const { categoryId } = route.params;
 
+  // Based on the category ID found in the route, retrieve information about this Category
   const selectedCategory = CATEGORIES.find(
     (category) => category.id === categoryId
   );
 
   // Select all meals that fit the current filter criteria
-  const availableMeals = useSelector((state) => state.meals.filteredMeals);
-  const mealsForSelectedCategory = availableMeals.filter((meal) =>
+  const filteredMeals = useSelector((state) => state.meals.filteredMeals);
+
+  // And then all the above meals that are in our current Category
+  const filteredMealsInCategory = filteredMeals.filter((meal) =>
     meal.categoryIds.includes(categoryId)
   );
 
   useEffect(() => {
-    // Name and style our navigation header according to the category selected
+    // Title and style our navigation header according to the category selected
     navigation.setOptions({
       title: selectedCategory.title,
       headerBackTitleStyle: { fontFamily: 'open-sans', fontSize: 10 },
@@ -26,9 +33,26 @@ const CategoryMealsScreen = ({ navigation, route }) => {
     });
   }, []);
 
+  if (!filteredMealsInCategory || filteredMealsInCategory.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>No meals found. Try again with fewer filters.</DefaultText>
+      </View>
+    );
+  }
+
   return (
-    <MealList listData={mealsForSelectedCategory} navigation={navigation} />
+    <MealList listData={filteredMealsInCategory} navigation={navigation} />
   );
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+});
 
 export default CategoryMealsScreen;
--- Section 05 - Navigation/rn-meals-app/screens/FavoritesScreen.js
+++ Section 05 - Navigation/rn-meals-app/screens/FavoritesScreen.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
 import { useSelector } from 'react-redux';
 import MealList from '../components/MealList';
+import DefaultText from '../components/DefaultText';
 
 const FavoritesScreen = ({ navigation }) => {
-  // TODO: Use the user's real list of Favorites, rather than hard-coding these two
   const favoriteMeals = useSelector((state) => state.meals.favoriteMeals);
+
+  // Render some fallback texts if we have no favorite meals yet
+  if (!favoriteMeals || favoriteMeals.length === 0) {
+    return (
+      <View style={styles.content}>
+        <DefaultText>
+          No favorite meals found. Browse meals and try adding some!
+        </DefaultText>
+      </View>
+    );
+  }
+
+  // Otherwise, render our list of favorite meals
   return <MealList listData={favoriteMeals} navigation={navigation} />;
 };
+
+const styles = StyleSheet.create({
+  content: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 15,
+  },
+});
 
 export default FavoritesScreen;
