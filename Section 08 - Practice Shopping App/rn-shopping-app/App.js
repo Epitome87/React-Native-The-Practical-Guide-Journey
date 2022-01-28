9993bc1d6ca660f26ebe7c4149141d83cfc96d4c
Matthew McGrath <rubywep@hotmail.com>
2022-01-27 20:13:20 -0800

    Create Redux store, with a products key to our products reducer and provide it to all components in App. Add font loading. Add a NavigationContainer and render the Products Stack.

--- Section 08 - Practice Shopping App/rn-shopping-app/App.js
+++ Section 08 - Practice Shopping App/rn-shopping-app/App.js
@@ -1,13 +1,39 @@
 import 'react-native-gesture-handler'; // Must be at top, for Navigation Drawers
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import productsReducer from './store/reducers/products';
+import { ProductsStack } from './navigation/ShopNavigator';
+import { NavigationContainer } from '@react-navigation/native';
+import { useFonts } from 'expo-font';
+
+const rootReducer = combineReducers({
+  products: productsReducer,
+});
+
+const store = createStore(rootReducer);
 
 export default function App() {
+  const [isLoaded] = useFonts({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+  });
+
+  if (!isLoaded) {
+    return (
+      <View>
+        <Text>Loading...</Text>
+      </View>
+    );
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style='auto' />
-    </View>
+    <Provider store={store}>
+      <NavigationContainer>
+        <ProductsStack />
+      </NavigationContainer>
+    </Provider>
   );
 }
 
