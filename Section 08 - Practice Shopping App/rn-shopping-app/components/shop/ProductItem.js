0e94d6f2a7e98d876fb4fa62e68d625b56ad4999
Matthew McGrath <rubywep@hotmail.com>
2022-01-28 17:07:11 -0800

    Wrap the returned JSX of ProductItem component with a TouchableOpacity or TouchableNativeFeedback based on the platform. Minor styling tweaks.

--- Section 08 - Practice Shopping App/rn-shopping-app/components/shop/ProductItem.js
+++ Section 08 - Practice Shopping App/rn-shopping-app/components/shop/ProductItem.js
@@ -1,26 +1,47 @@
-import { Button, Image, StyleSheet, View } from 'react-native';
+import {
+  Button,
+  Image,
+  Platform,
+  StyleSheet,
+  TouchableNativeFeedback,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 import React from 'react';
 import DefaultText from '../DefaultText';
 import colors from '../../constants/colors';
 
 const ProductItem = ({ title, price, image, onViewDetail, onAddToCart }) => {
-  return (
-    <View style={styles.product}>
-      <Image style={styles.image} source={{ uri: image }} />
-      <View style={styles.details}>
-        <DefaultText style={styles.title}>{title}</DefaultText>
-        <DefaultText style={styles.price}>${price.toFixed(2)}</DefaultText>
-      </View>
+  let TouchableComp = TouchableOpacity;
+  if (Platform.OS === 'android' && Platform.Version >= 21) {
+    TouchableComp = TouchableNativeFeedback;
+  }
 
-      <View style={styles.buttonsContainer}>
-        <Button
-          color={colors.primary}
-          title='View Details'
-          onPress={onViewDetail}
-        />
-        <Button color={colors.primary} title='To Cart' onPress={onAddToCart} />
+  return (
+    <TouchableComp onPress={onViewDetail} useForeground>
+      <View style={styles.product}>
+        <Image style={styles.image} source={{ uri: image }} />
+        <View style={styles.details}>
+          <DefaultText style={styles.title} bold>
+            {title}
+          </DefaultText>
+          <DefaultText style={styles.price}>${price.toFixed(2)}</DefaultText>
+        </View>
+
+        <View style={styles.buttonsContainer}>
+          <Button
+            color={colors.primary}
+            title='View Details'
+            onPress={onViewDetail}
+          />
+          <Button
+            color={colors.primary}
+            title='To Cart'
+            onPress={onAddToCart}
+          />
+        </View>
       </View>
-    </View>
+    </TouchableComp>
   );
 };
 
@@ -43,12 +64,14 @@ const styles = StyleSheet.create({
   },
   details: {
     alignItems: 'center',
+    justifyContent: 'center',
     height: '15%',
     padding: 10,
   },
   title: {
     fontSize: 18,
-    marginVertical: 4,
+    marginVertical: 2,
+    height: '100%', // This seems necessary to avoid price cutting it off for some odd reason
   },
   price: {
     fontSize: 14,
