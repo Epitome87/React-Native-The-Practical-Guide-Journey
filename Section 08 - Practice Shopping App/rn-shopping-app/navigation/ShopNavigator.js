b6de437c4fcf699760a6c205c1a4e43812961515
Matthew McGrath <rubywep@hotmail.com>
2022-01-28 17:01:16 -0800

    Provide style options to the header title and header back title to the default screen options.

--- Section 08 - Practice Shopping App/rn-shopping-app/navigation/ShopNavigator.js
+++ Section 08 - Practice Shopping App/rn-shopping-app/navigation/ShopNavigator.js
@@ -1,6 +1,7 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Platform } from 'react-native';
 import ProductsOverviewScreen from '../screens/ProductsOverviewScreen';
+import ProductDetailScreen from '../screens/ProductDetailScreen';
 import colors from '../constants/colors';
 
 const Stack = createNativeStackNavigator();
@@ -8,6 +9,12 @@ const Stack = createNativeStackNavigator();
 const defaultScreenOptions = {
   headerStyle: {
     backgroundColor: Platform.OS === 'android' ? colors.primary : '',
+  },
+  headerTitleStyle: {
+    fontFamily: 'open-sans-bold',
+  },
+  headerBackTitleStyle: {
+    fontFamily: 'open-sans',
   },
   headerTintColor: Platform.OS === 'android' ? 'white' : colors.primary,
 };
@@ -21,6 +28,16 @@ const ProductsStack = () => {
       <Stack.Screen
         name='Products Overview'
         component={ProductsOverviewScreen}
+        options={{
+          title: 'All Products',
+        }}
+      />
+      <Stack.Screen
+        name='Product Details'
+        component={ProductDetailScreen}
+        options={{
+          title: 'Product Details',
+        }}
       />
     </Stack.Navigator>
   );
